4055faec9b6650e704326a3ee2a91270efb6c4f2
Tom <43979399+TomBPotochek@users.noreply.github.com>
2020-10-12 20:57:39 -0300

    empezando a hacer andar SDL

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,71 @@
 #include <iostream>
 #include <SDL2/SDL.h>
-#include "res_path.h"
+//#include "res_path.h"
 
-/*
- * Lesson 1: Hello World!
- */
+const int ANCHO_VENTANA = 800;
+const int ALTO_VENTANA = 600;
+const int ERROR = -1;
+
+int inicializar_ventana(SDL_Window* win, SDL_Renderer* ren);
+
 int main(int, char**){
-	//First we need to start up SDL, and make sure it went ok
-	if (SDL_Init(SDL_INIT_VIDEO) != 0){
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return 1;
-	}
-	
-	//Now create a window with title "Hello World" at 100, 100 on the screen with w:640 h:480 and show it
-	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-	//Make sure creating our window went ok
-	if (win == nullptr){
-		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-		return 1;
-	}
+    
+    SDL_Window * ventana = nullptr;
+    SDL_Renderer * renderer = nullptr;
+    bool quit = false;
 
-	//Create a renderer that will draw to the window, -1 specifies that we want to load whichever
-	//video driver supports the flags we're passing
-	//Flags: SDL_RENDERER_ACCELERATED: We want to use hardware accelerated rendering
-	//SDL_RENDERER_PRESENTVSYNC: We want the renderer's present function (update screen) to be
-	//synchronized with the monitor's refresh rate
-	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (ren == nullptr){
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
-
-	//SDL 2.0 now uses textures to draw things but SDL_LoadBMP returns a surface
-	//this lets us choose when to upload or remove textures from the GPU
-	std::string imagePath = getResourcePath("helloworld") + "hello.bmp";
-	SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
-	if (bmp == nullptr){
-		SDL_DestroyRenderer(ren);
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
-
-	//To use a hardware accelerated texture for rendering we can create one from
-	//the surface we loaded
-	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
-	//We no longer need the surface
-	SDL_FreeSurface(bmp);
-	if (tex == nullptr){
-		SDL_DestroyRenderer(ren);
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
-	
-	//A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
-	for (int i = 0; i < 3; ++i){
-		//First clear the renderer
-		SDL_RenderClear(ren);
-		//Draw the texture
-		SDL_RenderCopy(ren, tex, NULL, NULL);
-		//Update the screen
-		SDL_RenderPresent(ren);
-		//Take a quick break after all that hard work
-		SDL_Delay(1000);
-	}
+    int valor = inicializar_ventana(ventana, renderer);
+    if (valor == ERROR)
+        quit = true;
+    
+    SDL_Event evento;
+    while (!quit){
+        SDL_RenderClear(renderer);
+        
+        SDL_RenderPresent(renderer);
+        
+        
+        while (SDL_PollEvent (&evento) != 0){
+            if (evento.type == SDL_KEYDOWN) {
+                switch(evento.key.keysym.sym){
+                    case (SDLK_ESCAPE):
+                      quit=true;
+                      break;
+            }
+        }
+            
+    }
+}
 
 	//Clean up our objects and quit
-	SDL_DestroyTexture(tex);
-	SDL_DestroyRenderer(ren);
-	SDL_DestroyWindow(win);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(ventana);
 	SDL_Quit();
 	
 	return 0;
 }
+
+int inicializar_ventana(SDL_Window* win, SDL_Renderer* ren){
+	if (SDL_Init(SDL_INIT_VIDEO) != 0){
+	   std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+	   return ERROR;
+    }
+    	
+	win = SDL_CreateWindow("Hermanos Mario", 100, 100, ANCHO_VENTANA, ALTO_VENTANA, SDL_WINDOW_SHOWN);
+	if (win == nullptr){
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		return ERROR;
+	}
+
+
+	ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == nullptr){
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return ERROR;
+	}
+    return 0;
+}
+
+
